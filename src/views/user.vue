c8e38328567ffc7e877c0761abf9b6997a1336b9
caojiantao <1649297435@qq.com>
2024-06-06 09:28:47 +0800

    init

--- src/views/user.vue
+++ src/views/user.vue
@@ -4,7 +4,7 @@
 <template>
  <div class="common-layout">
     <el-container>
-      <el-aside width="200px">Aside1</el-aside>
+      <el-aside width="200px">Asid2e1</el-aside>
       <el-container>
         <el-header>
           <Delete style="width: 1em; height: 1em; margin-right: 8px" /></el-header>
